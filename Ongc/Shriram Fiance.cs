4e90df1fa2f87ad8781cec3e828d2e38265d5a9e
sumit94165 <sumitPruthi94@gmail.com>
2023-03-14 21:04:59 +0530

    11
    11

--- Ongc/Shriram Fiance.cs
+++ Ongc/Shriram Fiance.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace Ongc
 {
@@ -11,15 +12,17 @@ namespace Ongc
         static void Main(string[] args)
         {
             //  string url = "https://corporate.whirlpoolindia.com/press-release/";
-            string url = "https://investor.cummins.com/news";
+            //  string url = "https://www.quesscorp.com/wp-content/themes/melos/media/press-release/pr-2022.php?_=1678725206223";
+            string url = "www.timetechnoplast.com/investor-center/financials-results/";
 
 
             string data = GetURLData(url);
             // whirlpool pressdata  NewsExtract(data, "//div[@class='post-title']");
             //Console.WriteLine(data);
-            // NewsExtract(data, "//li[@class='liscreen']");
-               NewsExtract(data, "//h2[@class='media-heading']");
-            //NewsExtract(data, " //li ");
+            //   NewsExtract(data, "//li[@class='col-md-6']");
+            //   NewsExtract(data, "//div[@class='Sdownload-link-box ele-animate-box bottom-to-top']");
+            //   NewsExtract(data, "//p[@class='result_box']");
+            NewsExtract(data, "//td ");
 
             Console.ReadLine();
         }
