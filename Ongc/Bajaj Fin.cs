dd983c24556511e5fb819041b3dfc7301f78b82a
sumit94165 <sumitPruthi94@gmail.com>
2023-03-23 12:41:24 +0530

    1
    1

--- Ongc/Bajaj Fin.cs
+++ Ongc/Bajaj Fin.cs
@@ -8,11 +8,11 @@ namespace Ongc
 {
     internal class Bajaj_Fin : BaseClass2
     {
-        static void Main(string[] args)
+        static void Main123(string[] args)
         {
             string url = "https://www.mmtclimited.com/pages/show/256-annual-report-(english)-";
             string data = GetURLData(url);
-            // NewsExtract(data, "//div[@class='action-box']");
+            // NewsExtract(data, "//div[@class='mc_box']");
             NewsExtract(data, "//li");
             Console.ReadLine();
         }
